363500790a9da1c13162af6c23684bd93f67ab48
Eric Schenk <eric.schenk@gmail.com>
2013-08-02 13:52:07 -0500

    Added functionality to pass params from session and redirect to remain RESTful.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  @@all_ratings = ['G','PG','PG-13','R','NC-17']
+  @@all_ratings = ['G','PG','PG-13','R']
 
   def get_all_ratings
     return @@all_ratings
@@ -12,9 +12,19 @@ class MoviesController < ApplicationController
   end
 
   def index
-    debugger
+    #set variable to track if params were updated with previous session
+    @param_updated = false
+    if not params[:sort].present? and session[:sort].present? and (params[:sort] != session[:sort])
+	params[:sort] = session[:sort]
+	@param_updated = true
+    end
+    if not params[:ratings].present? and session[:ratings].present? and (params[:ratings] != session[:ratings])
+	params[:ratings] = session[:ratings]
+	@param_updated = true
+    end
+
     @sort = params[:sort]
-    @all_ratings = get_all_ratings 
+    @all_ratings = get_all_ratings
     @selected_ratings = (params[:ratings].present? ? params[:ratings] : [])
     if not @selected_ratings.present?	#if no items are checked, set the filter to be based on all values
 	@selected_ratings = @all_ratings
@@ -22,7 +32,15 @@ class MoviesController < ApplicationController
 	@selected_ratings = @selected_ratings.flatten.keep_if{|v|v != "1"}
     end
     @movies = Movie.where(:rating => @selected_ratings).order("#{@sort}").find(:all)
+    
+    #update session to match params
+    session[:sort] = @sort
+    session[:ratings] = @selected_ratings
 
+    #redirect to remain RESTful with param values attached
+    if @param_updated 
+	redirect_to movies_path(:sort => params[:sort], :ratings => params[:ratings])
+    end
   end
 
   def new
@@ -32,6 +50,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
+    flash.keep
     redirect_to movies_path
   end
 
@@ -43,6 +62,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
     @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
+    flash.keep
     redirect_to movie_path(@movie)
   end
 
@@ -50,6 +70,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
+    flash.keep
     redirect_to movies_path
   end
 
